67a58a87393e767b6e56dfc25cbdddca2574a2c4
Amberly <armstrong.amberly@gmail.com>
2017-08-10 17:38:22 -0400

    testing mock interface

--- lib/vacation_station/cli.rb
+++ lib/vacation_station/cli.rb
@@ -2,6 +2,37 @@
 class VacationStation::CLI
 
 	def call
-		puts "Today's Destinations:"
+		list_deals
+		menu
+		goodbye
+	end
+
+	def list_deals
+		puts "Today's Destinations: "
+		puts <<-DOC.gsub /^\s+/, ""
+		1. Amelia Island, Florida
+		2. Ireland & Europe
+		3. Playa del Carmen
+		DOC
+	end
+
+	def menu
+			input = nil
+		while input != "exit"
+			puts "Enter the number of a destination to unlock the details of your deal (or type exit to quit)"
+			input = gets.strip.downcase
+			case input
+			when "1"
+				puts "price - summary"
+			when "2"
+				puts "price - summary"
+			when "3"
+				puts "price - summary"
+			end
+		end
+	end
+
+	def goodbye
+		puts "Thanks for checking out today's travel deals! Check back tomorrow for new deals!"
 	end
 end
