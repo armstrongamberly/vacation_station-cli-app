7718f4627958f0a0cd07fce25e77c1bc88142cd6
Amberly <armstrong.amberly@gmail.com>
2017-08-10 19:25:51 -0400

    mock interface updates

--- lib/vacation_station/cli.rb
+++ lib/vacation_station/cli.rb
@@ -8,31 +8,24 @@ class VacationStation::CLI
 	end
 
 	def list_deals
-		puts "Today's Destinations: "
-		puts "\n"
-		puts <<-DOC.gsub /^\s+/, ""
-		1. Amelia Island, Florida
-		2. Ireland & Europe
-		3. Playa del Carmen
-		DOC
+		@deals = VacationStation::Deal.today
+		@deals.each.with_index(1) do |deal, i|
+			puts "#{i}. #{deal}"
+		end
 	end
 
 	def menu
-			input = nil
+		input = nil
 		while input != "exit"
 			puts <<-DOC.gsub /^\s+/, ""
 			Enter the number of a destination to unlock the details of your deal!
 			type list to see your options again or type exit to quit)
 			DOC
 			input = gets.strip.downcase
-			case input
-			when "1"
-				puts "price - summary"
-			when "2"
-				puts "price - summary"
-			when "3"
-				puts "price - summary"
-			when "list"
+			
+			if input.to_i > 0
+				puts @deals[input.to_i - 1]
+			elsif input == "list"
 				list_deals
 			else
 				puts "I don't see that option here..."
--- vacation_station.gemspec
+++ vacation_station.gemspec
@@ -9,8 +9,8 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Amberly"]
   spec.email         = ["armstrong.amberly@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
+  spec.summary       = %q{Summarizes travel deals for a user}
+  spec.description   = %q{Asks the user to select a destination and then unlocks title, price, summary of that vacation deal.}
   spec.homepage      = "TODO: Put your gem's website or public repo URL here."
   spec.license       = "MIT"
 
