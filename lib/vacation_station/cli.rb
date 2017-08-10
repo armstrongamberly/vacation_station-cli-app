6c7ee80368f885507604daeafc0de76507d5a4ca
Amberly <armstrong.amberly@gmail.com>
2017-08-10 17:45:14 -0400

    mock interface update

--- lib/vacation_station/cli.rb
+++ lib/vacation_station/cli.rb
@@ -9,6 +9,7 @@ class VacationStation::CLI
 
 	def list_deals
 		puts "Today's Destinations: "
+		puts "\n"
 		puts <<-DOC.gsub /^\s+/, ""
 		1. Amelia Island, Florida
 		2. Ireland & Europe
@@ -19,7 +20,10 @@ class VacationStation::CLI
 	def menu
 			input = nil
 		while input != "exit"
-			puts "Enter the number of a destination to unlock the details of your deal (or type exit to quit)"
+			puts <<-DOC.gsub /^\s+/, ""
+			Enter the number of a destination to unlock the details of your deal!
+			type list to see your options again or type exit to quit)
+			DOC
 			input = gets.strip.downcase
 			case input
 			when "1"
@@ -28,6 +32,10 @@ class VacationStation::CLI
 				puts "price - summary"
 			when "3"
 				puts "price - summary"
+			when "list"
+				list_deals
+			else
+				puts "I don't see that option here..."
 			end
 		end
 	end
