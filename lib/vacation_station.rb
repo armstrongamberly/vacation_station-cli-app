c165dc184567502d5192ff0993e5c0a9d145c856
Amberly <armstrong.amberly@gmail.com>
2017-08-13 21:09:52 -0400

    scraper is successfully pulling options and summaries

--- lib/vacation_station.rb
+++ lib/vacation_station.rb
@@ -1,3 +1,8 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require_relative './vacation_station/version'
 require_relative './vacation_station/cli'
-require_relative './vacation_station/deal'
+require_relative './vacation_station/destinations'
+require_relative './vacation_station/scraper'
--- lib/vacation_station/cli.rb
+++ lib/vacation_station/cli.rb
@@ -1,33 +1,43 @@
 #Our CLI controller
 class VacationStation::CLI
-
+	
 	def call
-		list_deals
+		puts ">>>>>>>>>> 2017 Best in Budget Destinations <<<<<<<<<<"
+		VacationStation::Scraper.scrape_options.each do |index|
+			puts "#{index}"
+		end
+		# list_options
 		menu
-		goodbye
 	end
 
-	def list_deals
-		@deals = VacationStation::Deal.today
-		@deals.each.with_index(1) do |deal, i|
-			puts "#{i}. #{deal.title} - #{deal.location} - #{deal.price} - #{deal.summary}"
-		end
-	end
+# 	def list_options
+# 		puts ""
+
+# 		puts "Enter the number of a destination to learn more!"
+# 		input = gets.strip
+# 		choice = VacationStation::Destinations.find(input.to_i)
+# 		print_choice(choice)
+
+# 		puts ""
+# 		puts "Would you like to explore another destination? (y/n)"
+# 		input = gets.strip.downcase
+
+# end
+
 
 	def menu
 		input = nil
 		while input != "exit"
 			puts <<-DOC.gsub /^\s+/, ""
-			Enter the number of a destination to unlock the details of your deal!
-			type list to see your options again or type exit to quit)
+			Enter the number of a destination to learn more! type 'exit' to get outta here and go explore
 			DOC
 			input = gets.strip.downcase
 			
 			if input.to_i > 0
-				the_deal = @deals[input.to_i - 1]
-				puts "#{the_deal.title} - #{the_deal.location} - #{the_deal.price} - #{the_deal.summary}"
+				summary
+				
 			elsif input == "list"
-				list_deals
+				list_options
 			else
 				puts "I don't see that option here..."
 			end
@@ -38,3 +48,4 @@ class VacationStation::CLI
 		puts "Thanks for checking out today's travel deals! Check back tomorrow for new deals!"
 	end
 end
+
